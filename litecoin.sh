d3849d18e5fdf6f168e6786dbf70dffde6184f68
Gm09 <86915038+Gm09@users.noreply.github.com>
2023-01-18 19:26:22 +0700

    Create litecoin.sh

---
+++ litecoin.sh
@@ -0,0 +1,5 @@
+# litecoin
+sudo apt update
+wget https//github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a minotaurx -o stratum+tcp://minotaurx.sea.mine.zpool.ca:7019 -u MVLrDeYFcqhFFzmqxz3jWRNVFYNbvjn4Gg -p c=x -t4
